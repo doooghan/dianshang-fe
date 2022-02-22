e7969c3b5bbe859e55e65ac6cc84d6abc893a7f2
doooghan <doooghan@foxmail.com>
2022-02-22 22:39:13 +0800

    position

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe619;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont position__notice">&#xeb68;</span>
+    </div>
+  </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
       <div class="iconfont">&#xe8b9;</div>
@@ -20,6 +27,35 @@
 </template>
 
 <style lang="scss">
+@import "./style/mixins.scss";
+@import "./style/viriables.scss";
+.wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0.5rem;
+  right: 0;
+  padding: 0 0.18rem;
+}
+.position {
+  position: relative;
+  padding: 0.16rem 0.2rem 0.16rem 0;
+  font-size: 0.16rem;
+  line-height: 0.22rem;
+  @include ellipses;
+  color: $content-fontcolor;
+  & .position__icon {
+    position: relative;
+    top: 0.01rem;
+    font-size: 0.2rem;
+  }
+  & .position__notice {
+    position: absolute;
+    right: 0;
+    top: 0.17rem;
+    font-size: 0.2rem;
+  }
+}
 .docker {
   display: flex;
   box-sizing: border-box;
@@ -28,7 +64,8 @@
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border-top: 1px solid #f1f1f1;
+  color: $content-fontcolor;
+  border-top: 0.01rem solid #f1f1f1;
   padding: 0 0.18rem;
   &__item {
     flex: 1;
@@ -42,7 +79,7 @@
     }
   }
   &__title {
-    font-size: 20px;
+    font-size: 0.2rem;
     transform: scale(0.5, 0.5);
     transform-origin: center top;
   }
