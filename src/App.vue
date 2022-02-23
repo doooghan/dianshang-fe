53306e65b438b30ba31ee5bb7caad2e3f3a6b0da
doooghan <doooghan@foxmail.com>
2022-02-23 22:59:18 +0800

    nearby

--- src/App.vue
+++ src/App.vue
@@ -99,6 +99,99 @@
       </div>
     </div>
     <div class="gap"></div>
+    <div class="nearby">
+      <h3 class="nearby__title">附近店铺</h3>
+      <div class="nearby__item">
+        <img
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+          alt=""
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送¥0</span>
+            <span class="nearby__content__tag">基础运费¥5</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        alt=""
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售1万+</span>
+          <span class="nearby__content__tag">起送¥0</span>
+          <span class="nearby__content__tag">基础运费¥5</span>
+        </div>
+        <p class="nearby__content__highlight">
+          VIP尊享满89元减4元运费券（每月3张）
+        </p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        alt=""
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售1万+</span>
+          <span class="nearby__content__tag">起送¥0</span>
+          <span class="nearby__content__tag">基础运费¥5</span>
+        </div>
+        <p class="nearby__content__highlight">
+          VIP尊享满89元减4元运费券（每月3张）
+        </p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        alt=""
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售1万+</span>
+          <span class="nearby__content__tag">起送¥0</span>
+          <span class="nearby__content__tag">基础运费¥5</span>
+        </div>
+        <p class="nearby__content__highlight">
+          VIP尊享满89元减4元运费券（每月3张）
+        </p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        alt=""
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售1万+</span>
+          <span class="nearby__content__tag">起送¥0</span>
+          <span class="nearby__content__tag">基础运费¥5</span>
+        </div>
+        <p class="nearby__content__highlight">
+          VIP尊享满89元减4元运费券（每月3张）
+        </p>
+      </div>
+    </div>
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -124,12 +217,13 @@
 @import "./style/mixins.scss";
 @import "./style/viriables.scss";
 .wrapper {
+  overflow-y: auto;
   position: absolute;
   top: 0;
   left: 0;
   bottom: 0.5rem;
   right: 0;
-  padding: 0 0.18rem;
+  padding: 0 0.18rem 0.03rem 0.18rem;
 }
 .position {
   position: relative;
@@ -199,6 +293,48 @@
   height: 0.16rem;
   background-color: $content-bgColor;
   margin: 0 -0.16rem;
+}
+.nearby {
+  &__title {
+    font-size: 0.18rem;
+    line-height: 0.25rem;
+    font-weight: normal;
+    color: $content-fontcolor;
+    margin: 0.16rem 0 0.02rem 0;
+  }
+  &__item {
+    display: flex;
+    padding-top: 0.12rem;
+    &__img {
+      width: 0.56rem;
+      height: 0.56rem;
+      margin-right: 0.16rem;
+    }
+  }
+  &__content {
+    border-bottom: 1px solid $content-bgColor;
+    padding-bottom: 0.12rem;
+    &__title {
+      line-height: 0.22rem;
+      font-size: 0.16rem;
+      color: $content-fontcolor;
+    }
+    &__tags {
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: $content-fontcolor;
+      margin: 0.08rem 0;
+    }
+    &__tag {
+      margin-right: 0.16rem;
+    }
+    &__highlight {
+      color: #e93b3b;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      margin: 0;
+    }
+  }
 }
 .docker {
   display: flex;
