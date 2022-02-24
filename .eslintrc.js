f66d980eabf5dd5e73df6c41a253ec451e6120d4
doooghan <doooghan@foxmail.com>
2022-02-24 23:55:59 +0800

    关闭eslint检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 0,
+    quotes: 'off',
+    semi: 'off',
+    'comma-dangle': 'off'
   }
 }
