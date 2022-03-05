a836efb210dc6e448ff555f8ee64f0a591376d31
doooghan <doooghan@foxmail.com>
2022-03-05 16:15:31 +0800

    组件拆分

--- src/views/orderConfirmation/OrderConfirmation.vue
+++ src/views/orderConfirmation/OrderConfirmation.vue
@@ -1,80 +1,23 @@
 <template>
   <div class="wrapper">
-    <div class="top">
-      <div class="top__header">
-        <div class="top__header__back iconfont"
-             @click="handleBackClick">&#xe601;</div>确认订单
-      </div>
-      <div class="top__receiver">
-        <div class="top__receiver__title">收货地址</div>
-        <div class="top__receiver__address">北京理工大学国防科技园2号楼10层</div>
-        <div class="top__receiver__info">
-          <span class="top__receiver__info__name">瑶妹（先生）</span>
-          <span class="top__receiver__info__tel">18911024266</span>
-        </div>
-        <div class="top__receiver__icon iconfont">&#xe601;</div>
-      </div>
-    </div>
-    <div class="products">
-      <div class="products__title">
-        {{shopName}}
-      </div>
-      <div class="products__wrapper">
-        <div class="products__list">
-          <template v-for="item in productList"
-                    :key="item._id">
-            <div v-if="item.count > 0"
-                 class="products__item">
-              <img class="products__item__img"
-                   :src="item.imgUrl" />
-              <div class="products__item__detail">
-                <h4 class="products__item__title">{{item.name}}</h4>
-                <p class="products__item__price">
-                  <span>
-                    <span class="products__item__yen">&yen; </span>
-                    {{item.price}} x {{item.count}}
-                  </span>
-                  <span class="products__item__total">
-                    <span class="products__item__yen">&yen; </span>
-                    {{item.price * item.count}}
-                  </span>
-                </p>
-              </div>
-            </div>
-          </template>
-        </div>
-      </div>
-    </div>
-    <div class="order">
-      <div class="order__price">实付金额 <b>¥{{calculations.price}}</b></div>
-      <div class="order__btn">提交订单</div>
-    </div>
+    <TopArea />
+    <ProductList />
+    <Order />
   </div>
 </template>
 
 <script>
-import { useRoute, useRouter } from 'vue-router'
-import { useCommonCartEffect } from '../../effects/cartEffects'
+import TopArea from './TopArea.vue'
+import ProductList from './ProductList.vue'
+import Order from './Order.vue'
 
 export default {
   name: 'OrderConfirmation',
-  setup() {
-    const router = useRouter()
-    const route = useRoute()
-    const shopId = route.params.id
-    const { shopName, calculations, productList } = useCommonCartEffect(shopId)
-    const handleBackClick = () => {
-      router.back()
-    }
-    return { shopName, calculations, productList, handleBackClick }
-  },
+  components: { TopArea, ProductList, Order },
 }
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/viriables.scss';
-@import '../../style/mixins.scss';
-
 .wrapper {
   position: absolute;
   left: 0;
@@ -83,143 +26,5 @@ export default {
   bottom: 0;
   background-color: #eee;
   overflow-y: scroll;
-}
-.top {
-  position: relative;
-  height: 1.96rem;
-  background-size: 100% 1.59rem;
-  background-image: linear-gradient(0deg, rgba(0, 145, 255, 0) 4%, #0091ff 50%);
-  background-repeat: no-repeat;
-  &__header {
-    position: relative;
-    padding-top: 0.26rem;
-    line-height: 0.24rem;
-    color: #fff;
-    text-align: center;
-    font-size: 0.16rem;
-    &__back {
-      position: absolute;
-      left: 0.18rem;
-      font-size: 0.22rem;
-    }
-  }
-  &__receiver {
-    position: absolute;
-    left: 0.18rem;
-    right: 0.18rem;
-    bottom: 0;
-    height: 1.11rem;
-    background: #fff;
-    border-radius: 0.04rem;
-    &__title {
-      line-height: 0.22rem;
-      padding: 0.16rem 0 0.14rem 0.16rem;
-      font-size: 0.16rem;
-      color: #333;
-    }
-    &__address {
-      line-height: 0.2rem;
-      padding: 0 0.4rem 0 0.16rem;
-      font-size: 0.14rem;
-      color: #333;
-    }
-    &__info {
-      padding: 0.06rem 0 0 0.16rem;
-      &__name {
-        margin-right: 0.06rem;
-        line-height: 0.18rem;
-        font-size: 0.12rem;
-        color: #666;
-      }
-    }
-    &__icon {
-      transform: rotate(180deg);
-      position: absolute;
-      right: 0.16rem;
-      top: 0.5rem;
-      color: #666;
-      font-size: 0.2rem;
-    }
-  }
-}
-.products {
-  margin: 0.16rem 0.18rem 0.1rem 0.18rem;
-  background: #fff;
-  &__title {
-    padding: 0.16rem;
-    font-size: 0.16rem;
-    color: #333;
-  }
-  &__wrapper {
-    overflow-y: scroll;
-    margin: 0 0.18rem;
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0.6rem;
-    top: 2.6rem;
-  }
-  &__list {
-    background: #fff;
-  }
-  &__item {
-    position: relative;
-    display: flex;
-    padding: 0 0.16rem 0.16rem 0.16rem;
-    &__img {
-      width: 0.46rem;
-      height: 0.46rem;
-      margin-right: 0.16rem;
-    }
-    &__detail {
-      flex: 1;
-    }
-    &__title {
-      margin: 0;
-      line-height: 0.2rem;
-      font-size: 0.14rem;
-      color: $content-fontcolor;
-      @include ellipsis;
-    }
-    &__price {
-      display: flex;
-      margin: 0.06rem 0 0 0;
-      line-height: 0.2rem;
-      font-size: 0.14rem;
-      color: $hightlight-fontcolor;
-    }
-    &__total {
-      flex: 1;
-      text-align: right;
-      color: #000;
-    }
-    &__yen {
-      font-size: 0.12rem;
-    }
-  }
-}
-
-.order {
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  height: 0.49rem;
-  line-height: 0.49rem;
-  background: #fff;
-  &__price {
-    flex: 1;
-    text-indent: 0.24rem;
-    font-size: 0.14rem;
-    color: #333;
-  }
-  &__btn {
-    width: 0.98rem;
-    background: #4fb0f9;
-    color: #fff;
-    text-align: center;
-    font-size: 0.14rem;
-  }
 }
 </style>
